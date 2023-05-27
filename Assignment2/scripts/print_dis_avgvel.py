bf21f9ca7d7b7cd19446703299ce7ac2833f663e
issouker97 <65472009+issouker97@users.noreply.github.com>
2023-05-27 15:53:16 +0200

    Update print_dis_avgvel.py

--- Assignment2/scripts/print_dis_avgvel.py
+++ Assignment2/scripts/print_dis_avgvel.py
@@ -1,4 +1,25 @@
 #! /usr/bin/env python
+"""
+.. module:: print_dis_avgvel
+	:platform: Unix
+	:synopsis: Python module for the print_dis_avgvel
+
+.. moduleauthor:: KERMADJ YOUNES s5447235@studenti.unige.it
+
+
+Python module for the Subscriber node (print_dis_avgvel).
+
+This node for printing both the distance that separates the robot from the target and its average speed.
+In addition, It subscribes to the robot’s position and speed, this will be through using a custom message.
+A parameter will be used to set the frequency of publishing the data
+
+Subscribes to:
+
+- **/posxy_velxy** (message type: `Posxy_velxy`)
+
+---
+
+"""
 
 import rospy
 import math
@@ -10,6 +31,15 @@ from colorama import Fore, Back, Style
 
 class PrintInfo:
     def __init__(self):
+	 """
+    This function makes a new PrintInfo object. The object has three things:
+-  A number that tells how often to print the information.
+
+-  A number that tells when the information was last printed.
+
+-  A thing that listens to messages about the position and speed of the robot and does something with them..
+
+    """
         # Get the publish frequency parameter
         self.freq = rospy.get_param("frequency")
 
@@ -20,6 +50,11 @@ class PrintInfo:
         self.sub_pos = rospy.Subscriber("/posxy_velxy", Posxy_velxy, self.posvel_callback)
 
     def posvel_callback(self, msg):
+	"""
+        This function defines a method called posvel_callback that takes two parameters: self and msg. 
+        The method is used as a callback function for the subscriber object that listens to the topic /posxy_velxy. 
+  
+         """
         # Compute time period in milliseconds
         period = int((1.0 / self.freq) * 1000)
         
@@ -55,8 +90,17 @@ class PrintInfo:
 
 def main():
 	
+ """
+  This code defines a function called main that tries to suppress the timestamps from the log messages, 
+  initializes the node with the name print_dis_avgvel, 
+  creates an object of the PrintInfo class that subscribes to a topic and prints the distance and speed information of the robot at a given frequency, and waits for messages.
+  The code also calls the main function if the file is being run directly.
+  
+    """
+   
+	
     # Suppress the timestamps from the log messages
-    #rospy.set_param('/rosconsole/formatter/time', 'none') I tried to delete time values from screen but it didn't work	
+    #rospy.set_param('/rosconsole/formatter/time', 'none')	
     
     # Initialize the node
     rospy.init_node('print_dis_avgvel')
