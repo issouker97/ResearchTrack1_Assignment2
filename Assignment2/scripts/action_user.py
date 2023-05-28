6d4d3c0d58c4418e5aaeb5ff17cdb5f9dba20bc6
issouker97 <65472009+issouker97@users.noreply.github.com>
2023-05-28 23:38:46 +0200

    Update action_user.py

--- Assignment2/scripts/action_user.py
+++ Assignment2/scripts/action_user.py
@@ -67,12 +67,12 @@ def publisher(msg):
     pub.publish(posxy_velxy)
 
 def action_client():
-   """
-   This function creates an action client that communicates with an action server called /reaching_goal that provides a service of planning a path for the robot to reach a target pose.
-   The function asks the user to input the x and y coordinates of the target pose or type c to cancel the goal. 
-   If the user enters c, the function cancels the current goal. Otherwise, the function sends the goal to the action server and waits for feedback. 
+    """
+    This function creates an action client that communicates with an action server called /reaching_goal that provides a service of planning a path for the robot to reach a target pose.
+    The function asks the user to input the x and y coordinates of the target pose or type c to cancel the goal. 
+    If the user enters c, the function cancels the current goal. Otherwise, the function sends the goal to the action server and waits for feedback. 
 
-   """
+    """
     # create the action client
     action_client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2022.msg.PlanningAction)
     # wait for the server to be started
